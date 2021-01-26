9c80dfccc4598bd445be5106c61c88119aa47443
Nono <bruno.couvreur2020@campus-eni.fr>
2021-01-26 16:50:21 +0100

    TP 4 + javascript

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends  AbstractController
@@ -10,15 +11,22 @@ class MainController extends  AbstractController
      * @Route("/") , name="Main_home"
      */
 
-    public function home(){
-      return $this->render('main\home.html.twig');
+    public function home(): Response{
+      return $this->render('main\home.html.twig', []);
     }
 
     /**
-     * @Route ("/AboutUs") , name="Main_contact"
+     * @Route ("/about-us") , name="Main_contact"
      */
-    public function aboutus(){
-        return $this->render('main\About-us.html.twig');
+    public function aboutUs(): Response{
+        return $this->render('main\About-us.html.twig', []);
+    }
+
+    /**
+     * @Route ("/legal-stuff") , name="Main_legal-stuff"
+     */
+    public function legalStuff(): Response{
+        return $this->render('main\legal-stuff.html.twig', []);
     }
 }
 
--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -13,14 +13,16 @@ class WishController extends AbstractController
      */
     public function list(): Response
     {
-        return $this->render('wish/list.html.twig');
+        return $this->render('wish/list.html.twig',[]);
     }
 
     /**
-     * @Route("/wish/detail{id}", name="wish_detail", methods={"GET"}, requirements={"id": "[0-9]+"})
+     * @Route("/wish/detail/{id}", name="wish_detail", methods={"GET"}, requirements={"id": "[0-9]+"})
      */
     public function detail($id): Response
     {
-        return $this->render('detail/list.html.twig');
+        return $this->render('wish/detail.html.twig', [
+            "id" => $id
+        ]);
     }
 }
