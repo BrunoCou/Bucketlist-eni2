1e81084afaa6c4610d58be9027aa2bef2909e26c
Nono <bruno.couvreur2020@campus-eni.fr>
2021-01-26 14:37:55 +0100

    TP 4

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,10 +7,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends  AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/") , name="Main_home"
      */
 
     public function home(){
       return $this->render('main\home.html.twig');
     }
+
+    /**
+     * @Route ("/AboutUs") , name="Main_contact"
+     */
+    public function aboutus(){
+        return $this->render('main\About-us.html.twig');
+    }
 }
+
